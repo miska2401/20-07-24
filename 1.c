197049fc5706298238c9f159cbbcd40f29b72a0c
Samuel Arango <samuel.arango@upb.edu.co>
2024-07-20 18:01:53 -0500

    Added 1.c

---
+++ 1.c
@@ -0,0 +1,15 @@
+/* This is a comment */
+
+/**********************************************************
+* Name: pun.c *
+* Purpose: Prints a bad pun. *
+* Author: K. N. King *
+**********************************************************/
+
+#include <stdio.h>
+
+int main(void) /* Beginning of main program */
+{
+    printf("To C, or not to C: that is the question. \n");
+    return 0;
+}
